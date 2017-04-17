5aa8138cac87e31ec3b2d1383b380e6894d8b68e
Erik Bjäreholt <erik@bjareho.lt>
2017-04-17 14:22:23 +0200

    refactoring + created LockableDetector

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ class Detector:
     def __init__(self):
         self.client = ActivityWatchClient("status-checker")
 
-    def get_bucket_id(self, type):
-        # TODO: We need a better way to query buckets
+    def get_bucket_id(self, type: str):
         # TODO: Doesn't care about hostname
+        # TODO (maybe): Create a better way to query buckets
         buckets = self.client.get_buckets()
         # print(buckets)
         window_bucket = find(lambda bucket: bucket["type"] == type and "testing" not in bucket["id"], buckets.values())
@@ -31,29 +31,65 @@ class Detector:
         return window_bucket["id"]
 
     # TODO: Move to aw-client?
-    def get_last_event(self, bucket_id):
+    # TODO: Doesn't care if the event was old (as can happen if you have a dead watcher)
+    def get_last_event(self, bucket_id: str) -> Event:
         last_events = self.client.get_events(bucket_id, limit=1)
         if last_events:
             return last_events[0]
+        else:
+            raise Exception("no event found")
 
     def detect(self, bucket_id: str, filter_str: str) -> Optional[Event]:
         last_event = self.get_last_event(bucket_id)
-        if last_event is None:
-            raise Exception("no event found")
-        # print(last_event)
-
         return last_event if find(lambda label: filter_str in label.lower(), last_event.labels) else None
 
 
-if __name__ == "__main__":
-    activities = ["aw-detector", "fish", "vim", "chrome"]
+class LockableDetector(Detector):
+    """Identical to Detector, but provides a lock on the last event in a bucket to do multiple checks on the same event"""
 
-    detector = Detector()
+    def __init__(self, *args, **kwargs):
+        Detector.__init__(self, *args, **kwargs)
+        self.last_event_locked = False
+        self.locked_last_events = {}
+
+    def __enter__(self):
+        self.last_event_locked = True
+
+    def __exit__(self, *args, **kwargs):
+        self.last_event_locked = False
+        self.locked_last_events = {}
+
+    def get_last_event(self, bucket_id: str) -> Optional[Event]:
+        if self.last_event_locked:
+            if bucket_id not in self.locked_last_events:
+                self.locked_last_events[bucket_id] = Detector.get_last_event(self, bucket_id)
+            return self.locked_last_events[bucket_id]
+        else:
+            return Detector.get_last_event(self, bucket_id)
+
+
+example_activities = ["aw-detector", "fish", "vim", "chrome", "python"]
+
+
+def lockable_detector_example():
+    """
+    Without the lockable detector, a new currentwindow event right at the time of detection might invalidly
+    detect more than one "activity" since multiple calls to `detect` would do one event fetch each.
+
+    An alternative solution would be to write a new function `detect_many` that could be used to detect
+    multiple activities with a single event fetch.
+    """
+    detector = LockableDetector()
 
     window_bucket_id = detector.get_bucket_id(type="currentwindow")
     afk_bucket_id = detector.get_bucket_id(type="afkstatus")
 
     not_afk = detector.detect(afk_bucket_id, "not-afk")
-    for activity in activities:
-        if not_afk and detector.detect(window_bucket_id, activity):
-            print("{} seems to be active!".format(activity))
+    with detector:
+        for activity in example_activities:
+            if not_afk and detector.detect(window_bucket_id, activity):
+                print("{} seems to be active!".format(activity))
+
+
+if __name__ == "__main__":
+    lockable_detector_example()
